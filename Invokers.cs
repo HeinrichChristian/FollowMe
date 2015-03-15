6aec8dca3c5424c744dbeb97d9cc6cea763b1666
Christian Heinrich <heinrich.christian@gmx.net>
2015-03-15 19:17:59 +0100

    init
    just save the beginning of everything

---
+++ Invokers.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using EZ_B.WiimoteLib;
+
+public class Invokers {
+
+  delegate int GetTrackBarValueCallback(TrackBar control);
+  public static int GetTrackBarValue(TrackBar control) {
+
+    if (control.InvokeRequired) {
+
+      GetTrackBarValueCallback d = new GetTrackBarValueCallback(GetTrackBarValue);
+      return (int)control.Invoke(d, new object[] { control });
+    } else {
+
+      return control.Value;
+    }
+  }
+
+  delegate bool GetCheckedCallback(CheckBox control);
+  public static bool GetCheckedValue(CheckBox control) {
+
+    try {
+      if (control.InvokeRequired) {
+
+        GetCheckedCallback d = new GetCheckedCallback(GetCheckedValue);
+        return (bool)control.Invoke(d, new object[] { control });
+      } else {
+
+        return control.Checked;
+      }
+    } catch {
+
+      return false;
+    }
+  }
+
+  delegate string GetTextCallback(Control control);
+  public static string GetText(Control control) {
+
+    if (control.InvokeRequired) {
+
+      GetTextCallback d = new GetTextCallback(GetText);
+      return control.Invoke(d, new object[] { control }).ToString();
+    } else {
+
+      return control.Text;
+    }
+  }
+
+  delegate object SetComboBoxSelectedIndexCallback(ComboBox control, int index);
+  public static object SetComboBoxSelectedIndex(ComboBox control, int index) {
+
+    if (control.InvokeRequired) {
+
+      SetComboBoxSelectedIndexCallback d = new SetComboBoxSelectedIndexCallback(SetComboBoxSelectedIndex);
+      return control.Invoke(d, new object[] { control, index });
+    } else {
+
+      return control.SelectedIndex = index;
+    }
+  }
+
+  delegate object GetComboBoxSelectedItemCallback(ComboBox control);
+  public static object GetComboBoxSelectedItem(ComboBox control) {
+
+    if (control.InvokeRequired) {
+
+      GetComboBoxSelectedItemCallback d = new GetComboBoxSelectedItemCallback(GetComboBoxSelectedItem);
+      return control.Invoke(d, new object[] { control });
+    } else {
+
+      return control.SelectedItem;
+    }
+  }
+
+  //delegate Point GetPointToClientCallback(Form form, Point point);
+  //public static Point GetPointToClient(Form form, Point point) {
+
+  //  if (form.InvokeRequired) {
+
+  //    GetPointToClientCallback d = new GetPointToClientCallback(GetPointToClient);
+  //    return (Point)form.Invoke(d, new object[] { form, point });
+  //  } else {
+
+  //    return form.PointToClient(point);
+  //  }
+  //}
+
+  //delegate Rectangle GetRectangleToScreenCallback(Form form, Rectangle rect);
+  //public static Rectangle GetRectangleToScreen(Form form, Rectangle rect) {
+
+  //  if (form.InvokeRequired) {
+
+  //    GetRectangleToScreenCallback d = new GetRectangleToScreenCallback(GetRectangleToScreen);
+  //    return (Rectangle)form.Invoke(d, new object[] { form, rect });
+  //  } else {
+
+  //    return form.RectangleToScreen(rect);
+  //  }
+  //}
+
+  //delegate Point GetPointToScreenCallback(Form form, Point point);
+  //public static Point GetPointToScreen(Form form, Point point) {
+
+  //  if (form.InvokeRequired) {
+
+  //    GetPointToScreenCallback d = new GetPointToScreenCallback(GetPointToScreen);
+  //    return (Point)form.Invoke(d, new object[] { form, point });
+  //  } else {
+
+  //    return form.PointToScreen(point);
+  //  }
+  //}
+
+  delegate void SetFocusCallback(Control control);
+  public static void SetFocus(Control control) {
+
+    if (control.InvokeRequired) {
+
+      SetFocusCallback d = new SetFocusCallback(SetFocus);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Focus();
+    }
+  }
+
+  delegate void SetWindowFocusCallback(Form form);
+  public static void SetWindowFocus(Form form) {
+
+    if (form.InvokeRequired) {
+
+      SetWindowFocusCallback d = new SetWindowFocusCallback(SetWindowFocus);
+      form.Invoke(d, new object[] { form });
+    } else {
+
+      if (form.WindowState == FormWindowState.Minimized)
+        form.WindowState = FormWindowState.Maximized;
+
+      form.Activate();
+    }
+  }
+
+  delegate void SetToolStripTextCallback(ToolStrip control, string text);
+  public static void SetToolStripText(ToolStrip control, string text) {
+
+    if (control.InvokeRequired) {
+
+      SetToolStripTextCallback d = new SetToolStripTextCallback(SetToolStripText);
+      control.Invoke(d, new object[] { control, text });
+    } else {
+
+      control.Items[0].Text = text;
+    }
+  }
+
+  delegate void SetRefreshCallback(Control control);
+  public static void SetRefresh(Control control) {
+
+    if (control.InvokeRequired) {
+
+      SetRefreshCallback d = new SetRefreshCallback(SetRefresh);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Refresh();
+    }
+  }
+
+  delegate void SetTextCallback(Control control, object text, params object[] parameters);
+  public static void SetText(Control control, object text, params object[] parameters) {
+
+    try {
+
+      if (control.InvokeRequired) {
+
+        SetTextCallback d = new SetTextCallback(SetText);
+        control.Invoke(d, new object[] { control, text, parameters });
+      } else {
+
+        control.Text = string.Format(text.ToString(), parameters);
+      }
+    } catch {
+    }
+  }
+
+  delegate void SetEnabledCallback(Control control, bool enabled);
+  public static void SetEnabled(Control control, bool enabled) {
+
+    if (control.InvokeRequired) {
+
+      SetEnabledCallback d = new SetEnabledCallback(SetEnabled);
+      control.Invoke(d, new object[] { control, enabled });
+    } else {
+
+      control.Enabled = enabled;
+    }
+  }
+
+  delegate void SetItemAddCallback(ComboBox control, object item);
+  public static void SetItemAdd(ComboBox control, object item) {
+
+    if (control.InvokeRequired) {
+
+      SetItemAddCallback d = new SetItemAddCallback(SetItemAdd);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Items.Add(item);
+    }
+  }
+
+  delegate void SetItemsClearCallback(ComboBox control);
+  public static void SetItemsClear(ComboBox control) {
+
+    if (control.InvokeRequired) {
+
+      SetItemsClearCallback d = new SetItemsClearCallback(SetItemsClear);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Items.Clear();
+    }
+  }
+
+  delegate void SetVisibleCallback(Control control, bool visible);
+  public static void SetVisible(Control control, bool visible) {
+
+    if (control.InvokeRequired) {
+
+      SetVisibleCallback d = new SetVisibleCallback(SetVisible);
+      control.Invoke(d, new object[] { control, visible });
+    } else {
+
+      control.Visible = visible;
+    }
+  }
+
+  delegate void SetAppendTextCallback1(TextBox control, bool newLine, string text, params object[] param);
+  public static void SetAppendText(TextBox control, bool newLine, string text, params object[] param) {
+
+    try {
+
+      if (control.InvokeRequired) {
+
+        SetAppendTextCallback1 d = new SetAppendTextCallback1(SetAppendText);
+        control.Invoke(d, new object[] { control, newLine, text, param });
+      } else {
+
+        control.AppendText(string.Format(text, param));
+
+        if (newLine)
+          control.AppendText(Environment.NewLine);
+      }
+    } catch {
+    }
+  }
+
+  delegate void SetAppendTextCallback2(TextBox control, bool newLine, int maxLineCount, string text, params object[] param);
+  public static void SetAppendText(TextBox control, bool newLine, int maxLineCount, string text, params object[] param) {
+
+    if (control.InvokeRequired) {
+
+      SetAppendTextCallback2 d = new SetAppendTextCallback2(SetAppendText);
+      control.Invoke(d, new object[] { control, newLine, maxLineCount, text, param });
+    } else {
+
+      if (!control.IsDisposed && control.Lines.Length > maxLineCount)
+        control.Clear();
+
+      if (!control.IsDisposed)
+        control.AppendText(string.Format(text, param));
+
+      if (!control.IsDisposed && newLine)
+        control.AppendText(Environment.NewLine);
+    }
+  }
+
+  //delegate void SetForeColorCallback(Control control, Color foreColor);
+  //public static void SetForeColor(Control control, Color foreColor) {
+
+  //  try {
+
+  //    if (control.InvokeRequired) {
+
+  //      SetForeColorCallback d = new SetForeColorCallback(SetForeColor);
+  //      control.Invoke(d, new object[] { control, foreColor });
+  //    } else {
+
+  //      control.ForeColor = foreColor;
+  //    }
+  //  } catch {
+  //  }
+  //}
+
+  //delegate void SetLinkColorCallback(LinkLabel control, Color foreColor);
+  //public static void SetLinkColor(LinkLabel control, Color foreColor) {
+
+  //  if (control.InvokeRequired) {
+
+  //    SetLinkColorCallback d = new SetLinkColorCallback(SetLinkColor);
+  //    control.Invoke(d, new object[] { control, foreColor });
+  //  } else {
+
+  //    control.LinkColor = foreColor;
+  //  }
+  //}
+
+  //delegate void SetBackColorCallback(Control control, Color backColor);
+  //public static void SetBackColor(Control control, Color backColor) {
+
+  //  if (control.InvokeRequired) {
+
+  //    SetBackColorCallback d = new SetBackColorCallback(SetBackColor);
+  //    control.Invoke(d, new object[] { control, backColor });
+  //  } else {
+
+  //    control.BackColor = backColor;
+  //  }
+  //}
+
+  delegate void AddControlCallback(Control master, Control control);
+  public static void AddControl(Control master, Control control) {
+
+    if (control.InvokeRequired) {
+
+      AddControlCallback d = new AddControlCallback(AddControl);
+      control.Invoke(d, new object[] { master, control });
+    } else {
+
+      master.Controls.Add(control);
+    }
+  }
+
+  delegate void SetTabControlPageCallback(TabControl control, int pageIndex);
+  public static void SetTabControlPage(TabControl control, int pageIndex) {
+
+    if (control.InvokeRequired) {
+
+      SetTabControlPageCallback d = new SetTabControlPageCallback(SetTabControlPage);
+      control.Invoke(d, new object[] { control, pageIndex });
+    } else {
+
+      control.SelectedIndex = pageIndex;
+    }
+  }
+
+  delegate void SetProgressBarValueCallback(ProgressBar control, int value);
+  public static void SetProgressBarValue(ProgressBar control, int value) {
+
+    if (control.InvokeRequired) {
+
+      SetProgressBarValueCallback d = new SetProgressBarValueCallback(SetProgressBarValue);
+      control.Invoke(d, new object[] { control, value });
+    } else {
+
+      control.Value = value;
+    }
+  }
+
+  delegate void SetProgressBarIncrementCallback(ProgressBar control, int steps);
+  public static void SetProgressBarIncrement(ProgressBar control, int steps) {
+
+    if (control.InvokeRequired) {
+
+      SetProgressBarIncrementCallback d = new SetProgressBarIncrementCallback(SetProgressBarIncrement);
+      control.Invoke(d, new object[] { control, steps });
+    } else {
+
+      control.Increment(steps);
+      control.Update();
+    }
+  }
+
+  delegate void SetCheckedCallback(CheckBox control, bool isChecked);
+  public static void SetChecked(CheckBox control, bool isChecked) {
+
+    if (control.InvokeRequired) {
+
+      SetCheckedCallback d = new SetCheckedCallback(SetChecked);
+      control.Invoke(d, new object[] { control, isChecked });
+    } else {
+
+      control.Checked = isChecked;
+    }
+  }
+
+  delegate void SetOpacityCallback(Form form, double opacity);
+  public static void SetOpacity(Form form, double opacity) {
+
+    try {
+
+      if (form.InvokeRequired) {
+
+        SetOpacityCallback d = new SetOpacityCallback(SetOpacity);
+        form.Invoke(d, new object[] { form, opacity });
+      } else {
+
+        form.Opacity = opacity;
+      }
+    } catch {
+    }
+  }
+
+  delegate void ClearControlsback(Control control);
+  public static void ClearControls(Control control) {
+
+    if (control.InvokeRequired) {
+
+      ClearControlsback d = new ClearControlsback(ClearControls);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Controls.Clear();
+    }
+  }
+
+  delegate void DisposeControlback(Control control);
+  public static void DisposeControl(Control control) {
+
+    if (control.InvokeRequired) {
+
+      DisposeControlback d = new DisposeControlback(DisposeControl);
+      control.Invoke(d, new object[] { control });
+    } else {
+
+      control.Dispose();
+    }
+  }
+
+  delegate void ShowFormback(Form form);
+  public static void ShowForm(Form form) {
+
+    if (form.InvokeRequired) {
+
+      ShowFormback d = new ShowFormback(ShowForm);
+      form.Invoke(d, new object[] { form });
+    } else {
+
+      form.Show();
+    }
+  }
+
+  delegate void HideFormback(Form form);
+  public static void HideForm(Form form) {
+
+    if (form.InvokeRequired) {
+
+      HideFormback d = new HideFormback(HideForm);
+      form.Invoke(d, new object[] { form });
+    } else {
+
+      form.Hide();
+    }
+  }
+
+  delegate void ShowDialogFormback(Form form);
+  public static void ShowDialogForm(Form form) {
+
+    if (form.InvokeRequired) {
+
+      ShowDialogFormback d = new ShowDialogFormback(ShowDialogForm);
+      form.Invoke(d, new object[] { form });
+    } else {
+
+      form.ShowDialog();
+    }
+  }
+
+  delegate void CloseFormback(Form form);
+  public static void CloseForm(Form form) {
+
+    if (form.InvokeRequired) {
+
+      CloseFormback d = new CloseFormback(CloseForm);
+      form.Invoke(d, new object[] { form });
+    } else {
+
+      form.Close();
+    }
+  }
+
+  //delegate void SetDataGridViewRowTextBGColorCallback(DataGridView control, int rowNum, int cellNum, string text, Color bgColor);
+  //public static void SetDataGridViewRowTextBGColor(DataGridView control, int rowNum, int cellNum, string text, Color bgColor) {
+
+  //  if (control.InvokeRequired) {
+
+  //    SetDataGridViewRowTextBGColorCallback d = new SetDataGridViewRowTextBGColorCallback(SetDataGridViewRowTextBGColor);
+  //    control.Invoke(d, new object[] { control, rowNum, cellNum, text, bgColor });
+  //  } else {
+
+  //    control.Rows[rowNum].DefaultCellStyle.BackColor = bgColor;
+  //    control.Rows[rowNum].Cells[cellNum].Value = text;
+  //  }
+  //}
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EZ_B;
+using EZ_B.CameraDetection;
+
+namespace FollowMe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        float moveSensitivivivity = 0.20f;
+        int moveSleepTime = 400;
+        EZ_B.Joystick.Joystick _joystick = null;
+
+
+        Camera camera;
+        private System.Timers.Timer batteryTimer;
+        private readonly UCEZB_Connect ezB_Connect1 = new UCEZB_Connect();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ezB_Connect1.EZB.ShowDebugWindow();
+            camera = new Camera(ezB_Connect1.EZB);
+            camera.OnNewFrame += _camera_OnNewFrame;
+
+            
+
+            
+        }
+
+
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            camera.StopCamera();
+
+            base.OnClosing(e);
+           
+        }
+
+        void _camera_OnNewFrame()
+        {
+            camera.UpdatePreview();
+            try
+            {
+                ObjectLocation objectLocationByColor = camera.CameraBasicColorDetection.GetObjectLocationByColor(true, ColorDetection.ColorEnum.Red, 50, 80);
+
+                if (objectLocationByColor != null)
+                {
+                    Debug.WriteLine( objectLocationByColor.VerticalLocation.ToString());
+                    Dispatcher.Invoke(() => TextBoxXCoordinate.Text = objectLocationByColor.VerticalLocation.ToString());
+                        //.CenterX.ToString();
+                    Dispatcher.Invoke(() => TextBoxYCoordinate.Text = objectLocationByColor.HorizontalLocation.ToString());
+                        // CenterY.ToString();
+                }
+            }
+            catch (Exception exception)
+            {
+            }
+            
+            try
+            {
+                ObjectLocation objectLocationByQrCode = camera.CameraQRCodeDetection.GetObjectLocationByQRCode();
+
+                if (objectLocationByQrCode != null)
+                {
+                Dispatcher.Invoke(() =>     TextBoxQrCode.Text = objectLocationByQrCode.QRCodeText);
+                Debug.WriteLine(objectLocationByQrCode.QRCodeText);
+                }
+                else
+                {
+                    Dispatcher.Invoke(() => TextBoxQrCode.Text = string.Empty);
+                }
+
+
+                //    if (objectLocation.HorizontalLocation == ObjectLocation.HorizontalLocationEnum.Left)
+                //        btnLeft_Click(null, null);
+                //    else if (objectLocation.HorizontalLocation == ObjectLocation.HorizontalLocationEnum.Right)
+                //        btnRight_Click(null, null);
+                //}
+            }
+            catch (Exception e)
+            {
+                
+            }
+           
+        }
+
+        private  void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("Battery: " + ezB_Connect1.EZB.ARDrone.CurrentNavigationData.BatteryLevel);
+           Dispatcher.Invoke(() => TextBoxBattery.Text = ezB_Connect1.EZB.ARDrone.CurrentNavigationData.BatteryLevel.ToString());
+
+            
+        }
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            batteryTimer = new System.Timers.Timer();
+            batteryTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            batteryTimer.Interval = 5000;
+            batteryTimer.Enabled = true;
+
+            try
+            {
+                ezB_Connect1.EZB.ARDrone.Connect(EZ_B.ARDrone.ARDrone.ARDroneVersionEnum.V2);
+
+                if (ezB_Connect1.EZB.ARDrone.IsConnected)
+                {
+                    TextConnectionStatus.Text = "Verbunden";
+                }
+                else
+                {
+                    TextConnectionStatus.Text = "Nicht Verbunden";
+                }
+                Debug.WriteLine(ezB_Connect1.EZB.ARDrone.GetControlConfig());
+            }
+            catch(Exception exception)
+            {
+                TextConnectionStatus.Text = "Fehler";
+                MessageBox.Show("Fehler: " + exception.ToString());
+            }
+        }
+
+        private void ButtonShowCamera_Click(object sender, RoutedEventArgs e)
+        {
+            camera.StartCamera(
+                  new ValuePair(Camera.CAMERA_NAME_AR_DRONE, Camera.CAMERA_NAME_AR_DRONE),
+                  CameraPanel,
+                  320,
+                  240);
+
+            ezB_Connect1.EZB.ARDrone.StartVideo();
+        }
+
+        private void ButtonDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            ezB_Connect1.EZB.ARDrone.Disconnect();
+        }
+
+        private void ButtonBlinkLeds_Click(object sender, RoutedEventArgs e)
+        {
+            ezB_Connect1.EZB.ARDrone.PlayLedAnimation(EZ_B.ARDrone.Commands.LedAnimationEnum.BlinkRed, 2, 10);
+        }
+
+        private void ButtonRefreshJoysticks_Click(object sender, RoutedEventArgs e)
+        {
+        
+            if (_joystick != null) 
+            {
+                _joystick.Dispose();
+                _joystick = null;
+         //       Invokers.SetAppendText(textBox1, true, "Disconnected");
+            }
+
+            ComboBoxJoysticks.Items.Clear();
+            var availableDevices = EZ_B.Joystick.Joystick.GetAvailableDevices();
+            if (availableDevices != null)
+            {
+                foreach (var availableDevice in availableDevices)
+                {
+                    ComboBoxJoysticks.Items.Add(availableDevice);
+                }
+            }
+        }
+
+        private void ComboBoxJoysticks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EZ_B.Joystick.JoystickDevice jd = (EZ_B.Joystick.JoystickDevice)ComboBoxJoysticks.SelectedItem;
+            _joystick = new EZ_B.Joystick.Joystick(jd, ezB_Connect1.EZB);
+            _joystick.OnControllerAction += new EZ_B.Joystick.Joystick.OnJoystickMoveHandler(_joystick_OnControllerAction);
+            _joystick.StartEventWatcher();
+        }
+
+        void _joystick_OnControllerAction()
+        {
+            if (_joystick.ButtonPressed(0))
+            {
+                Dispatcher.Invoke(() => RadioButton1.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton1.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(2))
+            {
+                Dispatcher.Invoke(() => RadioButton2.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton2.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(3))
+            {
+                Dispatcher.Invoke(() => RadioButton3.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton3.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(4))
+            {
+                Dispatcher.Invoke(() => RadioButton4.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton4.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(5))
+            {
+                Dispatcher.Invoke(() => RadioButton5.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton5.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(6))
+            {
+                Dispatcher.Invoke(() => RadioButton6.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton6.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(7))
+            {
+                Dispatcher.Invoke(() => RadioButton7.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton7.IsChecked = false);
+            }
+
+            if (_joystick.ButtonPressed(8))
+            {
+                Dispatcher.Invoke(() => RadioButton8.IsChecked = true);
+            }
+            else
+            {
+                Dispatcher.Invoke(() => RadioButton8.IsChecked = false);
+            }
+
+
+            if (_joystick.AxisXStateChanged())
+            {
+                Dispatcher.Invoke(() => TextBoxJoystick.Text += _joystick.GetAxisX.ToString());
+            }
+
+            if (_joystick.AxisYStateChanged())
+            {
+                Dispatcher.Invoke(() => TextBoxJoystick.Text += _joystick.GetAxisY.ToString());
+            }
+        }
+    }
+}
