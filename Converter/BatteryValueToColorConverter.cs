8ef5d2145cdcaa5ebd760049b17fd1c50f2ecee9
Christian Heinrich <heinrich.christian@gmx.net>
2015-03-21 21:22:06 +0100

    value converter for batteryindicator color

---
+++ Converter/BatteryValueToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FollowMe.Converter
+{
+    [ValueConversion(typeof(int), typeof(Brushes))]   
+    public class BatteryValueToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var batteryValue = 0;
+            Int32.TryParse(value as string, out batteryValue);
+
+            if (batteryValue < 17)
+            {
+                return Brushes.Red;
+            }
+            else if(batteryValue < 40)
+            {
+                return Brushes.Orange;
+            }
+            return Brushes.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
