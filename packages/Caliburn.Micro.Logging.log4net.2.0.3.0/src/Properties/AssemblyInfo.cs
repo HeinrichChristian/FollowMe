fafe5ca0535f2d394f1adb448524585d337afbbb
Christian Heinrich <heinrich.christian@gmx.net>
2015-04-02 22:04:40 +0200

    fixing root dir

---
+++ packages/Caliburn.Micro.Logging.log4net.2.0.3.0/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Caliburn.Micro.Logging.log4net")]
+[assembly: AssemblyDescription("Logging for Caliburn.Micro.log4net(NET45)")]
+[assembly: AssemblyProduct("Caliburn.Micro.Logging.log4net")]
+[assembly: AssemblyCompany("David Buksbaum - http://buksbaum.us")]
+[assembly: AssemblyCopyright("Copyright © David Buksbaum 2009-2014")]
+
+[assembly: AssemblyConfiguration(".NET45")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDelaySignAttribute(false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b7388404-8b63-44a8-957a-b16e2acbbe0f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
